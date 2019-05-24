f2de7c84c4a571d7dd50addd3351b2bbfd193663
ZTao-z <1124693098@qq.com>
2019-05-24 14:37:04 +0800

    version 1

---
+++ 16340089_黄铸韬_HW8_v0/src/hw8.cpp
@@ -0,0 +1,292 @@
+// ImGUI
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+// GLEW/GLAD
+#include <glad/glad.h>
+// GLFW
+#include <GLFW/glfw3.h>
+// OTHER
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <deque>
+
+// homework
+#include "hw8.h"
+
+GLfloat currentPosX = 0, currentPosY = 0;
+std::deque<Point> pointList;
+float t = 0;
+
+void processMouseClick(GLFWwindow* window, int button, int action, int mods) {
+	if (button == GLFW_MOUSE_BUTTON_LEFT) {
+		if (action == GLFW_PRESS) {
+			pointList.push_back(Point(currentPosX, currentPosY));
+			t = 0;
+		}
+	}
+	else if(button == GLFW_MOUSE_BUTTON_RIGHT){
+		if (action == GLFW_PRESS) {
+			pointList.pop_back();
+			t = 0;
+		}
+	}
+}
+
+void processMousePosition(GLFWwindow* window, double xpos, double ypos) {
+	currentPosX = GLint(xpos);
+	currentPosY = GLint(ypos);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	// 当用户按下ESC键,我们设置window窗口的WindowShouldClose属性为true
+	// 关闭应用程序
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+// 创建顶点着色器
+const char* shader = "#version 330 core\n\
+						layout(location = 0) in vec3 pos;\n\
+						layout(location = 1) in vec3 color;\n\
+						out vec3 outColor;\n\
+						void main() {\n\
+							gl_Position = vec4(pos, 1.0);\n\
+							outColor = color;\n\
+						}\0";
+// 创建片段着色器
+const char* fragment = "#version 330 core\n\
+						in vec3 outColor;\n\
+						out vec4 color;\n\
+						void main() {\n\
+							color = vec4(outColor, 1.0f);\n\
+						}\0";
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
+int main() {
+	// 实例化GLFW窗口
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	// 创建一个窗口对象
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	// OpenGL渲染窗口的尺寸大小
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// 开始游戏循环之前注册函数至合适的回调(Esc退出)
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetMouseButtonCallback(window, processMouseClick);
+	glfwSetCursorPosCallback(window, processMousePosition);
+
+	// 初始化GLEW/GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// 初始化ImGUI
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsClassic();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 150");
+
+
+	/*绘制图形*/
+	//============================================
+	// 编译顶点着色器
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &shader, NULL);
+	glCompileShader(vertexShader);
+	// 检测是否成功编译
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR: " << infoLog << std::endl;
+	}
+
+	// 编译片段着色器
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragment, NULL);
+	glCompileShader(fragmentShader);
+	// 检测是否成功编译
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR: " << infoLog << std::endl;
+	}
+	// 创建着色器程序
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR: " << infoLog << std::endl;
+	}
+	// 删除着色器对象
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// ImGUI控制部分(颜色)
+
+	// 游戏循环
+	unsigned int VBO, VAO;
+	ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 1.00f);
+
+	Bresenham bresenham(WINDOW_WIDTH, WINDOW_HEIGHT);
+	Bezier bezier(WINDOW_WIDTH, WINDOW_HEIGHT);
+	bool show = false;
+	float lastTime = 0;
+	while (!glfwWindowShouldClose(window)) {
+		// 检查事件
+		glfwPollEvents();
+
+		float current = glfwGetTime();
+		float deltaTime = current - lastTime;
+		lastTime = current;
+
+		// 顶点坐标
+		GLfloat* vertices = NULL;
+		int total = 0;
+		std::vector<GLfloat> p;
+		std::vector<Point> v;
+		// 作图
+		// 点
+		for (int ptr = 0; ptr < pointList.size(); ptr++) {
+			for (int i = -3; i < 3; i++) {
+				for (int j = -3; j < 3; j++) {
+					bresenham.saveLinePoint(pointList[ptr].x+i, pointList[ptr].y+j, p);
+				}
+			}
+		}
+		// 线
+		if (pointList.size() >= 2) {
+			for (int ptr = 0; ptr < pointList.size() - 1; ptr++) {
+				bresenham.drawLine(pointList[ptr].x, pointList[ptr].y, pointList[ptr + 1].x, pointList[ptr + 1].y, p);
+			}
+		}
+		// bezier曲线
+		bezier.drawBezier(pointList, p);
+		// 过程
+		t += deltaTime * 0.1;
+		if (pointList.size() >= 3) {
+			std::deque<Point> tp = pointList;
+			for (int count = 0; count < pointList.size() - 1; count++) {
+				bezier.show(tp, t < 1.0 ? t : 1.0);
+				for (int ptr = 0; ptr < tp.size(); ptr++) {
+					for (int i = -3; i < 3; i++) {
+						for (int j = -3; j < 3; j++) {
+							bresenham.saveLinePoint(tp[ptr].x + i, tp[ptr].y + j, p);
+						}
+					}
+				}
+				for (int ptr = 0; ptr < tp.size() - 1; ptr++) {
+					bresenham.drawLine(tp[ptr].x, tp[ptr].y, tp[ptr + 1].x, tp[ptr + 1].y, p);
+				}
+			}
+		}
+		if (t >= 1.0f) {
+			t = 0.0f;
+		}
+
+		total = p.size();
+		vertices = new GLfloat[total];
+		int pos = 0;
+		for (unsigned int j = 0; j < p.size(); j++) {
+			vertices[pos++] = p[j];
+		}
+		
+		/*总流程*/
+		// 使用glGenBuffers函数和一个缓冲ID生成一个VBO对象
+		glGenBuffers(1, &VBO);
+		// 使用glGenBuffers函数和一个缓冲ID生成一个VAO对象
+		glGenVertexArrays(1, &VAO);
+		// 绑定VAO
+		glBindVertexArray(VAO);
+		// 缓冲类型:顶点缓冲
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		// 顶点数据复制到缓冲的内存中
+		glBufferData(GL_ARRAY_BUFFER, total*sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+		// 顶点属性指针
+		// 位置
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+		// 颜色
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
+		// 解除绑定
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		
+
+		// ImGUI生成
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		// 设置GUI窗口内容
+		
+		ImGui::Render();
+
+		int display_w, display_h;
+		glfwMakeContextCurrent(window);
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		// 清除窗口的移动轨迹
+		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		// 使用着色器程序
+		glUseProgram(shaderProgram);
+		// 绘制物体
+		glBindVertexArray(VAO);
+		// 绘制点
+		glDrawArrays(GL_POINTS, 0, total/6);
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwMakeContextCurrent(window);
+
+		// 交换缓冲
+		glfwSwapBuffers(window);
+
+		// 释放VBO，VAO，EBO
+		glDeleteVertexArrays(1, &VAO);
+		glDeleteBuffers(1, &VBO);
+
+		delete[] vertices;
+	}
+
+	// Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+	//============================================
+
+	// 释放GLFW分配的内存
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
---
+++ 16340089_黄铸韬_HW8_v0/src/hw8.h
@@ -0,0 +1,273 @@
+#ifndef bresenham_h
+#define bresenham_h
+
+// ImGUI
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+// GLEW/GLAD
+#include <glad/glad.h>
+// GLFW
+#include <GLFW/glfw3.h>
+// OTHER
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+
+class Bresenham {
+private:
+	const int WINDOW_WIDTH;
+	const int WINDOW_HEIGHT;
+public:
+	Bresenham(int w, int h) :WINDOW_WIDTH(w), WINDOW_HEIGHT(h) {};
+
+	// 保存直线上的点
+	void saveLinePoint(int temp_X, int temp_Y, std::vector<GLfloat>& out) {
+		GLfloat color = 1.0f;
+		out.push_back(GLfloat(float(temp_X) / float(WINDOW_WIDTH / 2)) - 1.0f);
+		out.push_back(1.0f - GLfloat(float(temp_Y) / float(WINDOW_HEIGHT / 2)));
+		out.push_back(0.0f);
+		for (int i = 0; i < 3; i++) {
+			out.push_back(color);
+		}
+	}
+	// 保存圆上的点
+	void saveCirclePoint(int temp_X, int temp_Y, std::vector<GLfloat>& out) {
+		GLfloat color = 1.0f;
+		out.push_back(GLfloat(float(temp_X) / float(WINDOW_WIDTH / 2)));
+		out.push_back(GLfloat(float(temp_Y) / float(WINDOW_HEIGHT / 2)));
+		out.push_back(0.0f);
+		for (int i = 0; i < 3; i++) {
+			out.push_back(color);
+		}
+	}
+	// Bresenham画直线
+	void drawLineWithBresenham(GLint x0, GLint y0,
+		GLint x1, GLint y1,
+		std::vector<GLfloat>& out) {
+		int dx = abs(x1 - x0);
+		int dy = abs(y1 - y0);
+		int direct_x = x1 > x0 ? 1 : -1;
+		int direct_y = y1 > y0 ? 1 : -1;
+		int p = 0;
+
+		if (dx > dy) {
+			p = 2 * dy - dx;
+			while (x0 != x1) {
+				this->saveLinePoint(x0, y0, out);
+				if (p > 0) {
+					y0 += direct_y;
+					p = p + 2 * (dy - dx);
+				}
+				else {
+					p = p + 2 * dy;
+				}
+				x0 += direct_x;
+			}
+		}
+		else {
+			p = 2 * dx - dy;
+			while (y0 != y1) {
+				this->saveLinePoint(x0, y0, out);
+				if (p > 0) {
+					x0 += direct_x;
+					p = p + 2 * (dx - dy);
+				}
+				else {
+					p = p + 2 * dx;
+				}
+				y0 += direct_y;
+			}
+		}
+	}
+	// Bresenham画圆
+	void drawCircleWithBresenham(int center_x, int center_y, int radius, std::vector<GLfloat>& out) {
+		int x = 0, y = radius, d = 3 - 2 * radius;
+		while (x <= y) {
+			// 八点法画圆
+			this->saveCirclePoint( x + center_x,  y + center_y, out);
+			this->saveCirclePoint( x + center_x, -y + center_y, out);
+			this->saveCirclePoint(-y + center_x,  x + center_y, out);
+			this->saveCirclePoint(-y + center_x, -x + center_y, out);
+			this->saveCirclePoint(-x + center_x, -y + center_y, out);
+			this->saveCirclePoint(-x + center_x,  y + center_y, out);
+			this->saveCirclePoint( y + center_x, -x + center_y, out);
+			this->saveCirclePoint( y + center_x,  x + center_y, out);
+			if (d < 0) {
+				d = d + 4 * x + 6;
+			}
+			else {
+				d = d + 4 * (x - y) + 10;
+				y--;
+			}
+			x++;
+		}
+	}
+	// 计算直线一般式：Ax+By+C=0
+	std::vector<int> lineEquation(GLint X1, GLint Y1, GLint X2, GLint Y2) {
+		std::vector<int> res;
+		int A = Y2 - Y1,
+			B = X1 - X2,
+			C = X2 * Y1 - X1 * Y2;
+		res.push_back(A);
+		res.push_back(B);
+		res.push_back(C);
+		return res;
+	}
+	// 使用Edge Equation方法填充颜色
+	void fillTriangleUsingEdgeEquation(GLint Xa, GLint Ya,
+		GLint Xb, GLint Yb,
+		GLint Xc, GLint Yc,
+		std::vector<GLfloat>& out) {
+		// 获取外接矩形
+		int max_x = std::max(Xa, std::max(Xb, Xc)),
+			max_y = std::max(Ya, std::max(Yb, Yc)),
+			min_x = std::min(Xa, std::min(Xb, Xc)),
+			min_y = std::min(Ya, std::min(Yb, Yc));
+		// 获取三角形三条边
+		std::vector<std::vector<int>> lines;
+		lines.push_back(this->lineEquation(Xa, Ya, Xb, Yb));
+		lines.push_back(this->lineEquation(Xb, Yb, Xc, Yc));
+		lines.push_back(this->lineEquation(Xc, Yc, Xa, Ya));
+		// 确定直线方程的方向
+		for (int i = 0; i < 3; i++) {
+			GLint x_t, y_t;
+			if (i == 0) {
+				x_t = Xc; y_t = Yc;
+			}
+			else if (i == 1) {
+				x_t = Xa; y_t = Ya;
+			}
+			else {
+				x_t = Xb; y_t = Yb;
+			}
+			if (lines[i][0] * x_t + lines[i][1] * y_t + lines[i][2] < 0) {
+				for (int k = 0; k < 3; k++) {
+					lines[i][k] *= -1;
+				}
+			}
+		}
+		// 填充颜色
+		for (GLint x = min_x; x <= max_x; x++) {
+			for (GLint y = min_y; y <= max_y; y++) {
+				bool inside = true;
+				for (int i = 0; i < lines.size(); i++) {
+					if (lines[i][0] * x + lines[i][1] * y + lines[i][2] < 0) {
+						inside = false;
+						break;
+					}
+				}
+				if (inside) {
+					this->saveLinePoint(x, y, out);
+				}
+			}
+		}
+	}
+
+	// 画直线
+	void drawLine(GLint Xa, GLint Ya,
+				  GLint Xb, GLint Yb,
+		          std::vector<GLfloat>& res) {
+		drawLineWithBresenham(Xa, Ya, Xb, Yb, res);
+	}
+
+	// 画三角形
+	void drawTriangle(GLfloat Xa, GLfloat Ya,
+					  GLfloat Xb, GLfloat Yb,
+					  GLfloat Xc, GLfloat Yc,
+					  std::vector<GLfloat>& res,
+					  bool fullfill = false) {
+		GLint Xa_t = (GLint)std::floor((Xa + 1)*WINDOW_WIDTH) / 2,
+			Ya_t = (GLint)std::floor((1 - Ya)*WINDOW_HEIGHT) / 2,
+			Xb_t = (GLint)std::floor((Xb + 1)*WINDOW_WIDTH) / 2,
+			Yb_t = (GLint)std::floor((1 - Yb)*WINDOW_HEIGHT) / 2,
+			Xc_t = (GLint)std::floor((Xc + 1)*WINDOW_WIDTH) / 2,
+			Yc_t = (GLint)std::floor((1 - Yc)*WINDOW_HEIGHT) / 2;
+		drawLineWithBresenham(Xa_t, Ya_t, Xb_t, Yb_t, res);
+		drawLineWithBresenham(Xb_t, Yb_t, Xc_t, Yc_t, res);
+		drawLineWithBresenham(Xc_t, Yc_t, Xa_t, Ya_t, res);
+
+		if (fullfill) {
+			fillTriangleUsingEdgeEquation(Xa_t, Ya_t, Xb_t, Yb_t, Xc_t, Yc_t, res);
+		}
+	}
+	// 画圆
+	void drawCircle(GLfloat x, GLfloat y, int radius, std::vector<GLfloat>& res) {
+		int center_x = std::floor(x * WINDOW_WIDTH / 2),
+			center_y = std::floor(y * WINDOW_HEIGHT / 2);
+		drawCircleWithBresenham(center_x, center_y, radius, res);
+	}
+};
+
+struct Point {
+	GLint x, y;
+	Point(GLint a, GLint b) {
+		x = a;
+		y = b;
+	}
+};
+
+typedef struct Point Point;
+
+class Bezier {
+public:
+private:
+	const int WINDOW_WIDTH;
+	const int WINDOW_HEIGHT;
+public:
+	Bezier(int w, int h) :WINDOW_WIDTH(w), WINDOW_HEIGHT(h) {};
+
+	// 保存直线上的点
+	void savePoint(float temp_X, float temp_Y, std::vector<GLfloat>& out) {
+		GLfloat color = 1.0f;
+		out.push_back(GLfloat(float(temp_X) / float(WINDOW_WIDTH / 2)) - 1.0f);
+		out.push_back(1.0f - GLfloat(float(temp_Y) / float(WINDOW_HEIGHT / 2)));
+		out.push_back(0.0f);
+		for (int i = 0; i < 3; i++) {
+			out.push_back(color);
+		}
+	}
+
+	void drawBezier(std::deque<Point>& points, std::vector<GLfloat>& out) {
+		if (points.size() <= 2) {
+			return;
+		}
+		for (int t = 0; t <= 1000; t++) {
+			double x = 0, y = 0;
+			for (int i = 0; i < points.size(); i++) {
+				x += C(points.size()-1, i) * pow(0.001*t, i) * pow((1.0 - 0.001*t), points.size()-1 - i) * points[i].x;
+				y += C(points.size()-1, i) * pow(0.001*t, i) * pow((1.0 - 0.001*t), points.size()-1 - i) * points[i].y;
+			}
+			savePoint(x, y, out);
+		}
+	}
+
+	void show(std::deque<Point>& points, float t) {
+		if (points.size() <= 1) {
+			return;
+		}
+		std::deque<Point> v;
+		for (int i = 0; i < points.size()-1; i++) {
+			Point temp(int(t * points[i+1].x + (1 - t) * (points[i].x) + 0.5), int(t * points[i+1].y + (1 - t) * (points[i].y) + 0.5));
+			v.push_back(temp);
+		}
+		points = v;
+	}
+
+	int C(int n, int m) {
+		if (m == 0 || n == m) return 1;
+		int sb = std::min(m, n - m);
+		int f = 1, f1 = 0;
+		for (int i = 1; i <= sb; i++){
+			f1 = f * (n - i + 1) / (i);
+			f = f1;
+		}
+		return f1;
+	}
+};
+
+#endif // !bresenham_h
